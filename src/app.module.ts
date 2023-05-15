c12ce46dec42f105f5b7271d439d466d9a210791
ariesdimasy <ariesdimasy@gmail.com>
2023-05-15 22:16:39 +0700

    inject depedency service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LatihanModule } from './latihan/latihan.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [LatihanModule],
+  imports: [LatihanModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
