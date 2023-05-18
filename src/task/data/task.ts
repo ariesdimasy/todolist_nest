b228255137dd76fcf340a332cfd7599f6646b60f
ariesdimasy <ariesdimasy@gmail.com>
2023-05-18 11:27:05 +0700

    add task data

---
+++ src/task/data/task.ts
@@ -0,0 +1,17 @@
+interface Task {
+  task_id: number;
+  task_name: string;
+  task_description: string;
+}
+export const task: Task[] = [
+  {
+    task_id: 1,
+    task_name: 'Task 1',
+    task_description: 'Task 1 description',
+  },
+  {
+    task_id: 2,
+    task_name: 'Task 2',
+    task_description: 'Task 2 description',
+  },
+];
